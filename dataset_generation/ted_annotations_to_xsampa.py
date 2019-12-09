41f5ed8ac6bc44bad923b7dd70571f8cc67d3a89
padmanabhankrishnamurthy <pkaa@connect.ust.hk>
2019-12-09 17:37:11 +0530

    Fixed word to x-sampa phoneme conversion. Some words not converted due to wiktionary errors

--- dataset_generation/ted_annotations_to_xsampa.py
+++ dataset_generation/ted_annotations_to_xsampa.py
@@ -6,6 +6,7 @@ Convert ground truth words to their XSAMPA phoneme equivalents
 from pprint import pprint
 import ssl
 import os
+from os import path
 from pywiktionary import Wiktionary
 
 #resolve SSL Certificate Verification Failure
@@ -15,34 +16,46 @@ wikt = Wiktionary(XSAMPA=True)
 # word = wikt.lookup("apple")
 # pprint(word)
 
-dataset_path = '/Users/padmanabhankrishnamurthy/PycharmProjects/helen_v2p/lrs3_annotations/test/'
+dataset_path = '/Users/padmanabhankrishnamurthy/Desktop/lrs3/lrs3_annotations/test/'
+phoneme_annotations_path = '/Users/padmanabhankrishnamurthy/Desktop/lrs3/lrs3_annotations/test_phonemes/'
 for directory in os.listdir(dataset_path):
+    directory_name = directory
+    if not path.exists(phoneme_annotations_path + directory_name):
+        os.mkdir(phoneme_annotations_path + directory_name)
     directory = os.path.join(dataset_path, directory)
     if '.DS_Store' not in directory:
         for txt_file in os.listdir(directory):
+            txt_file_name = txt_file[:txt_file.find('.txt')]
             txt_file = os.path.join(directory, txt_file)
-            with open(txt_file, 'r') as file:
-                line = file.readline() #reads only first line
-                print(line[line.find(':')+2:], end=': ')
-                with open(txt_file[:txt_file.find('.txt')] + '_phoneme' + '.txt', 'w') as new_file:
-                    phoneme_translation = ''
-                    for word in line.split()[1:]:
-                        word = word.lower()
-                        wikt_word = wikt.lookup(word)
-                        phonemes = []
-                        for dict in wikt_word['English']:
-                            if dict['X-SAMPA'] != 'en':
-                                phonemes.append(dict['X-SAMPA'])
-                        #if '@' figures in any of the phoneme transcriptions, that transcription is probably the accurate one; ignore others
-                        phoneme = phonemes[0]
-                        for element in phonemes:
-                            if '@' in element:
-                                phoneme = element
-                                break
-                        phoneme_translation+=phoneme
-                        print(phoneme)
-                    new_file.write(phoneme_translation)
-            print('===============')
+            print("Processing {}".format(txt_file))
+            if '.DS_Store' not in txt_file:
+                with open(txt_file, 'r') as file:
+                    line = file.readline() #reads only first line
+                    print(line[line.find(':')+2:], end=': ')
+                    with open(phoneme_annotations_path + directory_name + '/' + txt_file_name + '.txt', 'a') as new_file:
+                        if os.stat(phoneme_annotations_path + directory_name + '/' + txt_file_name + '.txt').st_size == 0:
+                            phoneme_translation = ''
+                            for word in line.split()[1:]:
+                                word = word.lower()
+                                wikt_word = wikt.lookup(word)
+                                phonemes = []
+                                if type(wikt_word) == dict and 'English' in wikt_word.keys():
+                                    for phoneme_dict in wikt_word['English']:
+                                        if type(phoneme_dict) == dict and 'X-SAMPA' in phoneme_dict.keys() and phoneme_dict['X-SAMPA'] != 'en':
+                                            phonemes.append(phoneme_dict['X-SAMPA'])
+                                    #if '@' figures in any of the phoneme transcriptions, that transcription is probably the accurate one; ignore others
+                                    if phonemes:
+                                        phoneme = phonemes[0]
+                                        for element in phonemes:
+                                            if '@' in element:
+                                                phoneme = element
+                                                break
+                                        phoneme_translation+=phoneme
+                                        print(phoneme)
+                            new_file.write(phoneme_translation)
+                        else:
+                            print('Phoneme transcription already exists at {}'.format(phoneme_annotations_path + directory_name + '/' + txt_file_name + '.txt'))
+                print('===============')
 
 
 
